d737d938f836d57ef26b598546b82639c5c29a93
Juan Cicka <juanmcicka@gmail.com>
2023-10-08 14:25:05 -0300

    varias correcciones

---
+++ js/detalle.js
@@ -0,0 +1,39 @@
+//Para que amplien sus conocimientos sobre el objeto location
+//https://www.w3schools.com/js/js_window_location.asp
+//https://developer.mozilla.org/en-US/docs/Web/API/Location
+//Aquí guardo lo que trae el query strings
+let codigo = location.search;
+console.log(codigo);
+//Aquí uso esta instancia para lograr ver mejor los datos del query strings
+let codigoProducto = new URLSearchParams(codigo);
+console.log(codigoProducto);
+//Aquí guardo el código que el usuario seleccionó
+let codigoSeleccionado = codigoProducto.get('idDrink');
+console.log(codigoSeleccionado);
+
+//Capturo los datos de la página de detalle donde quiero a futuro mostrar los datos del producto seleccionado
+let codigoFinal = document.getElementById('codigo');
+let nombre = document.getElementById('nombre');
+let imagen = document.getElementById('imagen');
+
+//Tomo los datos del localStorage
+let detalleProducto = JSON.parse(localStorage.getItem('detallesProducto'))
+//Convierto el objeto literal a un array
+let arrayDetalle = JSON.parse(detalleProducto[0]);
+console.log(arrayDetalle);
+
+//Muestro de manera dinámica los detalles del producto que tengo en el localStorage
+codigoFinal.innerHTML = `Código del Producto: ${codigoSeleccionado}`;
+nombre.innerHTML = `Nombre del producto : ${arrayDetalle.strDrink}`;
+imagen.innerHTML = `<img class="w-100" src="${arrayDetalle.strDrinkThumb}" alt="${arrayDetalle.strDrink}"/>`
+
+//Capturo el botón que me permite regresar y además borrar todo lo que tengo en mi localStorage
+let botonRegresar = document.getElementById('botonRegresar');
+botonRegresar.addEventListener('click', function(){
+    //Este a mi no me funcionó
+    //localStorage.removeItem('detalleProducto');
+    //Pero este sí -Perfectamente borra todo lo que está en el localStorage
+    localStorage.clear()
+    //Aquí retorno a la página principal y muestro todos los datos que estan en el archivo json
+    location.href = 'index.html'
+})
--- js/master.js
+++ js/master.js
@@ -13,10 +13,45 @@ fetch('../datos/productos.json')
             <img class='w-100'src="${producto.strDrinkThumb}" alt="">
             <h3>Nombre: ${producto.strDrink}</h3>
             <h4>Categoria: ${producto.strCategory}</h4>
-            <a class='btn btn-outline-danger d-block' href='#'> Ver detalle</a>
+            <a id='${JSON.stringify(producto)}'class='btn btn-outline-danger d-block botonDetalle' href='#'> Ver detalle</a>
         </article>
         `
-    });
+    })
+    let botonDetalle = document.querySelectorAll('.botonDetalle')
+    //console.log(botonDetalle);
+    //Aquí guardaré los datos del producto seleccionado - pero en forma array de objetos
+    let arrayMiListaDeProductos
+    //Aquí guardo el objeto pero convertido a un array - Para poder tomar el código que el usuario seleccionó
+    let miObjeto
+    //Aquí es donde voy a guardar el código del productos que el usuario seleccionó
+    let codigo
+    //Aquí construyo está lógica para que poder dejar en escucha, a ver cual elemento selecciona el usuario para ver el detalle
+
+    botonDetalle.forEach(productoSeleccion => {
+         productoSeleccion.addEventListener('click', function(e){
+             //La eqtiqueta ancla por defecto tiene un evento enviar los datos
+             e.preventDefault()
+             //Traigo datos del localStorage - No es necesario haerlo en este caso propuesto
+             let miListaDeProductos = localStorage.getItem('detallesProducto')
+             if(miListaDeProductos == null){
+                 arrayMiListaDeProductos = [];
+             }else{
+                 arrayMiListaDeProductos = JSON.parse(miListaDeProductos)
+                 //console.log(arrayMiListaDeProductos);
+             }
+            //Aquí estoy guardando en el array de forma completa el objeto 
+            arrayMiListaDeProductos.push(this.id)
+            //Hago esto para poder converir el objeto seleccionado en un array
+            miObjeto = JSON.parse(arrayMiListaDeProductos[0])
+            //Aquí tomo el código que el usuario seleccionó
+            codigo = miObjeto.idDrink
+            //Aquí guardo en el localStorage el objeto que usuario seleccionó
+            localStorage.setItem('detallesProducto', JSON.stringify(arrayMiListaDeProductos))
+            //Aquí construyo mi Query strings - Lo cual es la ruta a donde debo enviar al usuario y además junto a ello le envío el código que fué seleccionado
+            location.href = `detalle.html?idDrink=${codigo}`
+             
+         })    
+     } )
 })
 .catch((error)=>{
     console.log('Uff ha ocurrido un error '+error)
